6d9236e482dfe229af8cfc84de67f6ec8ee28bdf
william.dunlop <william.dunlop@holler.com.au>
2019-02-22 15:36:18 +1100

    resolved package vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-                presets: ['react', 'es2015', 'stage-1']
+                presets: ['react', 'es2015']
             }
         }, {
             test: /\.scss$/,
